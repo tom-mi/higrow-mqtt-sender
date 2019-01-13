9acb6079671eb3061f6b75bfc194b2fb6c43eed0
Thomas Reifenberger <tom-mi@users.noreply.github.com>
2019-01-13 10:19:46 +0100

    Fix wifi connection timeout

--- src/wifi.cpp
+++ src/wifi.cpp
@@ -5,17 +5,16 @@
 const int WIFI_CONNECT_TIMEOUT_SECONDS = 30;
 
 boolean connect_wifi() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
   Serial.print(F("Connecting to wifi "));
   Serial.println(WIFI_SSID);
-
-  for (int i=0; i<WIFI_CONNECT_TIMEOUT_SECONDS || WiFi.status() != WL_CONNECTED; i++) {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  for (int i=0; i<WIFI_CONNECT_TIMEOUT_SECONDS && WiFi.status() != WL_CONNECTED; i++) {
     delay(1000);
     Serial.print(".");
   }
+  Serial.println("");
   if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("");
     Serial.print(F("Connected to "));
     Serial.println(WIFI_SSID);
     Serial.print(F("IP address: "));
